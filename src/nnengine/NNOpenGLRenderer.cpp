898721975c2758e26086cd4c8781141e3da91d2b
kciter <kciter@naver.com>
2014-02-19 00:48:18 +0900

    * NNOpenGLRenderer Init

---
+++ src/nnengine/NNOpenGLRenderer.cpp
@@ -0,0 +1,7 @@
+
+#include "NNOpenGLRenderer.h"
+
+namespace NNEngine
+{
+
+}
---
+++ src/nnengine/NNOpenGLRenderer.h
@@ -0,0 +1,19 @@
+
+/*
+ * NNOpenGLRenderer.h
+ * 작성자: 이선협
+ * 작성일: 2014. 02. 18
+ * 마지막으로 수정한 사람:
+ * 수정일:
+ * Direct3D 9.0 SDK를 사용하여 만들어진 렌더러 클래스
+ */
+
+#ifndef __NNOPENGLRENDERER_H__
+#define __NNOPENGLRENDERER_H__
+
+namespace NNEngine
+{
+
+}
+
+#endif
