196a9ca18c435be56ab4534add59bed38c31f54c
kciter <kciter@naver.com>
2014-02-18 21:41:59 +0900

    * Added D3D Library

--- src/nnengine/NNLibSet.h
+++ src/nnengine/NNLibSet.h
@@ -11,6 +11,21 @@
 #ifndef __NNLIBSET_H__
 #define __NNLIBSET_H__
 
+#if defined(_WIN32) || defined(_WIN64) /* Windows */
+
+/* timeGetTime */
 #pragma comment(lib, "winmm.lib")
+/* // */
+
+/* D3D */
+#pragma comment(lib, "d3d9.lib")
+#ifdef _DEBUG
+#pragma comment(lib, "d3dx9d.lib")
+#else
+#pragma comment(lib, "d3dx9.lib")
+#endif
+/* // */
+
+#endif
 
 #endif
