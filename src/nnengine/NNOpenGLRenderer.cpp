38799b4350c942e62fc071c737d680c07f6746a4
kciter <kciter@naver.com>
2014-02-19 22:32:12 +0900

    * Create NNOpenGLRenderer Class

--- src/nnengine/NNOpenGLRenderer.cpp
+++ src/nnengine/NNOpenGLRenderer.cpp
@@ -1,7 +1,51 @@
 
+/*
+ * NNOpenGLRenderer.cpp
+ * 작성자: 이선협
+ * 작성일: 2014. 02. 19
+ * 마지막으로 수정한 사람:
+ * 수정일:
+ * OpenGL을 사용하여 만들어진 렌더러 클래스
+ */
+
 #include "NNOpenGLRenderer.h"
 
 namespace NNEngine
 {
+	NNOpenGLRenderer::NNOpenGLRenderer()
+	{
+	}
+	NNOpenGLRenderer::~NNOpenGLRenderer()
+	{
+	}
 
+	bool NNOpenGLRenderer::Init()
+	{
+		return true;
+	}
+	bool NNOpenGLRenderer::Release()
+	{
+		return true;
+	}
+	bool NNOpenGLRenderer::DrawBegin()
+	{
+		glBegin( GL_POINTS );
+		return true;
+	}
+	bool NNOpenGLRenderer::DrawEnd()
+	{
+		glEnd();
+		glFlush();
+		return true;
+	}
+	bool NNOpenGLRenderer::Clear()
+	{
+		glClearColor( 0.f, 0.f, 0.f, 1.f );
+		glClear( GL_COLOR_BUFFER_BIT );
+		return true;
+	}
+	bool NNOpenGLRenderer::ToggleFullscreen()
+	{
+		return true;
+	}
 }
--- src/nnengine/NNOpenGLRenderer.h
+++ src/nnengine/NNOpenGLRenderer.h
@@ -2,18 +2,69 @@
 /*
  * NNOpenGLRenderer.h
  * 작성자: 이선협
- * 작성일: 2014. 02. 18
+ * 작성일: 2014. 02. 19
  * 마지막으로 수정한 사람:
  * 수정일:
- * Direct3D 9.0 SDK를 사용하여 만들어진 렌더러 클래스
+ * OpenGL을 사용하여 만들어진 렌더러 클래스
  */
 
 #ifndef __NNOPENGLRENDERER_H__
 #define __NNOPENGLRENDERER_H__
 
+#include "NNBaseRenderer.h"
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <Windows.h>
+#endif
+
+#include <GL/GL.h>
+#include <GL/GLU.h>
+
 namespace NNEngine
 {
+	class NNOpenGLRenderer : public NNBaseRenderer
+	{
+	public:
+		NNOpenGLRenderer();
+		virtual ~NNOpenGLRenderer();
 
+	public:
+		/* Init
+		 * Return Type: bool
+		 * NNRenderer에서 상속받아 오버라이딩
+		 */
+		bool Init();
+
+		/* Release
+		 * Return Type: bool
+		 * NNRenderer에서 상속받아 오버라이딩
+		 */
+		bool Release();
+
+		/* DrawBegin
+		 * Return Type: bool
+		 * NNRenderer에서 상속받아 오버라이딩
+		 */
+		bool DrawBegin();
+
+		/* DrawEnd
+		 * Return Type: bool
+		 * NNRenderer에서 상속받아 오버라이딩
+		 */
+		bool DrawEnd();
+
+		/* Clear
+		 * Return Type: bool
+		 * NNRenderer에서 상속받아 오버라이딩
+		 */
+		bool Clear();
+
+		/* ToggleFullscreen
+		 * Return Type: bool
+		 * NNRenderer에서 상속받아 오버라이딩
+		 */
+		bool ToggleFullscreen();
+	};
 }
 
 #endif
